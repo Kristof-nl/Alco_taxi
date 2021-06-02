9217e3508274ef39da94bf208bca6ba7fd7f7a5a
Krzysztof <hermaszewski.krzysztof@gmail.com>
2021-06-03 01:30:11 +0200

    Start with forms

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET KEY'] = '7y90Rzj87kjl5t5r195rty'
+
 
 @app.route('/')
 def home():
@@ -15,6 +19,24 @@ def beer():
 @app.route('/wine')
 def wine():
     return render_template("wine.html")
+
+
+@app.route('/strong')
+def strong():
+    return render_template("strong.html")
+
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 
 if __name__ == "__main__":
